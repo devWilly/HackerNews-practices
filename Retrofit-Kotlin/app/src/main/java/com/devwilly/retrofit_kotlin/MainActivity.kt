f002710d5ee0f8d6a30717541851ef198479f6ea
Willy Chang <ms70831@gmail.com>
2017-11-21 22:25:50 +0800

    implement getTopStories to TopStoriesActivity

--- Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/MainActivity.kt
+++ Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/MainActivity.kt
@@ -1,77 +1,23 @@
 package com.devwilly.retrofit_kotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Html
-import android.widget.TextView
-import com.devwilly.retrofit_kotlin.api.ApiService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
-
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
-    val API_SERVICE_URL = " https://hacker-news.firebaseio.com/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val storiesTextId: TextView = findViewById(R.id.stories_text_id)
-        val sb = StringBuffer()
-
-        var topStoriesListResponse: Call<List<Int>> = getApiService().getTopStories()
-        topStoriesListResponse.enqueue(object: Callback<List<Int>> {
-            override fun onResponse(call: Call<List<Int>>?, response: Response<List<Int>>?) {
-                var list: List<Int>? = response?.body()
-                sb.append(Html.fromHtml(getString(R.string.top_stories)))
-
-                for (item in list!!) {
-                    sb.append('\n').append(item)
-                }
-
-                storiesTextId.text = sb.toString()
-
-
-            }
-
-            override fun onFailure(call: Call<List<Int>>?, t: Throwable?) {
-
-            }
-        })
-
-        val newStoriesListResponse: Call<List<Int>> = getApiService().getNewStories()
-        newStoriesListResponse.enqueue(object: Callback<List<Int>>{
-            override fun onResponse(call: Call<List<Int>>?, response: Response<List<Int>>?) {
-                var list: List<Int>? = response?.body()
-                sb.append('\n').append(getString(R.string.new_stories))
-
-                for (item in list!!) {
-                    sb.append('\n').append(item)
-                }
-
-                storiesTextId.text = sb.toString()
-            }
-
-            override fun onFailure(call: Call<List<Int>>?, t: Throwable?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        })
-
+        findViewById<Button>(R.id.top_story_btn).setOnClickListener({ openActivityPage(TopStoriesActivity::class.java) })
 
     }
 
-    private fun getRetrofit(): Retrofit {
-        return Retrofit.Builder()
-                .baseUrl(API_SERVICE_URL)
-                .addConverterFactory(GsonConverterFactory.create()).build()
-    }
-
-    private fun getApiService(): ApiService {
-        return getRetrofit().create(ApiService::class.java)
+    private fun openActivityPage(cls: Class<*>) {
+        val intent = Intent()
+        intent.setClass(this@MainActivity, cls)
+        startActivity(intent)
     }
 }
---
+++ Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/api/ApiClient.kt
@@ -0,0 +1,20 @@
+package com.devwilly.retrofit_kotlin.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by Willy on 21/11/2017.
+ */
+class ApiClient {
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl("https://hacker-news.firebaseio.com/")
+                .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    fun getApiService(): ApiService {
+        return getRetrofit().create(ApiService::class.java)
+    }
+}
