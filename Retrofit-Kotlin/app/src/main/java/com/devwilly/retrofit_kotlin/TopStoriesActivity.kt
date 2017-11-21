66cd6a281f2d8600ff03eede9614b1f93fbb9311
Willy Chang <ms70831@gmail.com>
2017-11-21 22:25:06 +0800

    create sample activity page

---
+++ Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/TopStoriesActivity.kt
@@ -0,0 +1,15 @@
+package com.devwilly.retrofit_kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Willy on 21/11/2017.
+ */
+class TopStoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.item_story_layout)
+    }
+}
