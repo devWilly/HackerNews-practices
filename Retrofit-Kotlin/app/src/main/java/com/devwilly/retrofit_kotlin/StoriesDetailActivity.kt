33c3b6c91cedab2c03d7b04d68e214b879fb76e6
Willy Chang <ms70831@gmail.com>
2017-11-22 22:15:45 +0800

    implement to story detail api data

--- Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/StoriesDetailActivity.kt
+++ Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/StoriesDetailActivity.kt
@@ -2,6 +2,12 @@ package com.devwilly.retrofit_kotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.devwilly.retrofit_kotlin.api.ApiClient
+import com.devwilly.retrofit_kotlin.model.StoryDetailModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * Created by Willy on 21/11/2017.
@@ -11,5 +17,35 @@ class StoriesDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.item_story_layout)
+
+        val storyTitle = findViewById<TextView>(R.id.story_text)
+        val sb = StringBuffer()
+
+        ApiClient().getApiService().getTopStories().enqueue(object : Callback<List<Int>>{
+            override fun onResponse(call: Call<List<Int>>?, response: Response<List<Int>>?) {
+                val list = response?.body()
+
+                if (list != null) {
+                    // give five list item to test story detail api
+                    val sampleList = listOf(list[0], list[1], list[2], list[3], list[4], list[5])
+                    sampleList.forEach { index ->
+                        ApiClient().getApiService().getStoryDetail(index)
+                                .enqueue(object : Callback<StoryDetailModel> {
+                                    override fun onResponse(call: Call<StoryDetailModel>?, response: Response<StoryDetailModel>?) {
+                                        val item = response?.body()
+                                        sb.append(item?.title).append('\n')
+
+                                        storyTitle.text = sb.toString()
+                                    }
+
+                                    override fun onFailure(call: Call<StoryDetailModel>?, t: Throwable?) {
+                                    }
+                                }) }
+                }
+            }
+
+            override fun onFailure(call: Call<List<Int>>?, t: Throwable?) {
+            }
+        })
     }
 }
--- Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/api/ApiService.kt
+++ Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/api/ApiService.kt
@@ -1,7 +1,9 @@
 package com.devwilly.retrofit_kotlin.api
 
+import com.devwilly.retrofit_kotlin.model.StoryDetailModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 /**
  * Created by Willy on 20/11/2017.
@@ -17,4 +19,7 @@ interface ApiService {
 
     @GET("v0/beststories.json?print=pretty")
     fun getBestStories(): Call<List<Int>>
+
+    @GET("v0/item/{id}.json?print=pretty")
+    fun getStoryDetail(@Path("id") id: Int): Call<StoryDetailModel>
 }
