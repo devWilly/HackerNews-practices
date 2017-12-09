2d65b2dc72d2cce2fede766bae1563d24eb1aa5f
Willy Chang <ms70831@gmail.com>
2017-12-09 14:47:11 +0800

    create BaseItemViewHolder and TopStoryItemViewHolder extend BaseItemViewHolder

---
+++ Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/viewholder/BaseItemViewHolder.kt
@@ -0,0 +1,9 @@
+package com.devwilly.retrofit_kotlin.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Created by Willy on 09/12/2017.
+ */
+open class BaseItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
--- Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/viewholder/TopStoryItemViewHolder.kt
+++ Retrofit-Kotlin/app/src/main/java/com/devwilly/retrofit_kotlin/viewholder/TopStoryItemViewHolder.kt
@@ -1,6 +1,5 @@
 package com.devwilly.retrofit_kotlin.viewholder
 
-import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 import com.devwilly.retrofit_kotlin.R
@@ -8,6 +7,6 @@ import com.devwilly.retrofit_kotlin.R
 /**
  * Created by Willy on 05/12/2017.
  */
-class TopStoryItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class TopStoryItemViewHolder(itemView: View) : BaseItemViewHolder(itemView) {
     val text: TextView = itemView.findViewById(R.id.item_story_text)
 }
